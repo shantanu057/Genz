9670e427ab4ce1845ba521cd032b8a3e002eb51a
HrGoutham <hrgoutham678@gmail.com>
2022-09-12 09:59:01 +0530

    routing change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './Modules/authentication/login/login.component';
-import { SignUpComponent } from './Modules/authentication/sign-up/sign-up.component';
-import { DashboardComponent } from './Modules/dashboard/dashboard/dashboard.component';
 
-const routes: Routes = [
-  {path:'login',component:LoginComponent},
-  {path:'registration',component:SignUpComponent},
-  {path:'dashboard',component:DashboardComponent}
-];
+
+const routes: Routes = []
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
