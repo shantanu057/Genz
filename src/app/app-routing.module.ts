4379e0087bcf3634f78948f8bce91e989665dcdd
HrGoutham <hrgoutham678@gmail.com>
2022-09-12 09:23:04 +0530

    Updated Login and SignUp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Modules/authentication/login/login.component';
+import { SignUpComponent } from './Modules/authentication/sign-up/sign-up.component';
+import { DashboardComponent } from './Modules/dashboard/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'registration',component:SignUpComponent},
+  {path:'dashboard',component:DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import { AuthenticationModule } from './Modules/authentication/authentication.module';
+import { HttpClientModule } from '@angular/common/http';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+
+
+
+
 
 @NgModule({
   declarations: [
@@ -11,8 +21,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    AppRoutingModule,MatFormFieldModule,
+    BrowserAnimationsModule,MatInputModule,FormsModule,ReactiveFormsModule,AuthenticationModule,HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
